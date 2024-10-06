404b4a7782262f7f7684233a46cd6a0423cc75ce
Nada Sulaiman | Ejada Systems <nsulaiman@ejada.com>
2024-10-06 16:46:44 +0300

    Add edit game/add game component

---
+++ Models/Genre.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace GameStore.Frontend.Models;
+
+public class Genre
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+
+}
--- obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e571fe5cefab0bdb375df3e650c1736e840919f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc68c3f1f24ceef7198f530e2cfae17a88dab401")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
