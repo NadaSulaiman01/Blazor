a550d2ccf87e031c56f6de4678107a3e93bb36c3
Nada Sulaiman | Ejada Systems <nsulaiman@ejada.com>
2024-10-08 17:17:31 +0300

    Add interactive server side rendering

--- obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29ffc6643e42c9ae3b934422f7ec0232d653f597")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a22c466fae4eeaac7f7e4b82a40b30f80962838")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
